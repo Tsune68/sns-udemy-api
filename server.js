c8cbb2522d55f9e8e678804f8d23a63c0368d731
恒川泰智 <tsunekawataichi@Hajimari-TsunekawaTaichi.local>
2024-03-06 01:57:25 +0900

    新規登録とログインのAPIを作成

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
+const authRoute = require("./routers/auth");
+
+require("dotenv").config();
 
 const PORT = 8000;
+
+app.use(express.json());
+
+//authRouteのエンドポイントの先頭に"/api/auth"が付くようになる。
+app.use("/api/auth", authRoute);
 
 app.listen(PORT, () => console.log(`server is running on PORT ${PORT}`));
